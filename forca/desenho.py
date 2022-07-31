b0dc590b83aee0c8461c53f133c5538fe4620537
Felipe Cardozo <97531117+felipecardozz@users.noreply.github.com>
2022-07-31 11:52:37 -0300

    Create desenho.py

---
+++ forca/desenho.py
@@ -0,0 +1,60 @@
+# Como o arquivo não fica numa janela tipo a do Pygame, precisa de um "desenho" no terminal, o que funciona igual a variável lista no forca.py so que mais organizado 
+
+enforcamento = {
+    0: """
+                ___________
+               | /        | 
+               |/        ( )
+               |          |
+               |         / \\
+               |
+           """,
+    1: """
+                ___________
+               | /        | 
+               |/        ( )
+               |          |
+               |         / 
+               |
+            """,
+    2: """
+                ___________
+               | /        | 
+               |/        ( )
+               |          |
+               |          
+               |
+            """,
+    3: """
+                ___________
+               | /        | 
+               |/        ( )
+               |          
+               |          
+               |
+            """,
+    4: """
+                ___________
+               | /        | 
+               |/        
+               |          
+               |          
+               |
+            """,
+    5: """
+                ___________
+               | /        
+               |/        
+               |          
+               |          
+               |
+            """,
+    6: """
+               |
+               |
+               |
+               |
+               |
+            """,
+    7: "",
+}
